af6ee44532a2678378942c6c164c65334da9d061
Glenn <glennake.1@gmail.com>
2020-04-13 14:10:23 +0100

    use predefined indentation levels in fortigate generator

--- OpenFireVert/generators/fortigate.py
+++ OpenFireVert/generators/fortigate.py
@@ -27,20 +27,26 @@ def generate(logger, parsed_data):
 
     dst_config = []
 
+    cfglvl1 = "  "
+    cfglvl2 = "    "
+    cfglvl3 = "      "
+    cfglvl4 = "        "
+    cfglvl5 = "          "
+
     # Generate system
 
     logger.log(2, __name__ + ": generate system")
 
     if "hostname" in parsed_data["system"]:
         dst_config.append("config system global")
-        dst_config.append("  set hostname " + parsed_data["system"]["hostname"])
+        dst_config.append(cfglvl1 + "set hostname " + parsed_data["system"]["hostname"])
         dst_config.append("end")
     else:
         logger.log(3, __name__ + ": hostname not found in parsed data")
 
     if "domain" in parsed_data["system"]:
         dst_config.append("config system dns")
-        dst_config.append("  set domain " + parsed_data["system"]["domain"])
+        dst_config.append(cfglvl1 + "set domain " + parsed_data["system"]["domain"])
         dst_config.append("end")
     else:
         logger.log(3, __name__ + ": domain name not found in parsed data")
@@ -55,42 +61,49 @@ def generate(logger, parsed_data):
 
         if attributes["type"] == "interface":
 
-            dst_config.append("  edit " + interface)
-            dst_config.append("    set vdom root")
+            dst_config.append(cfglvl1 + "edit " + interface)
+            dst_config.append(cfglvl2 + "set vdom root")
 
             if attributes["ipv4_config"]:
 
-                dst_config.append("    set mode static")
+                dst_config.append(cfglvl2 + "set mode static")
 
-                for ipv4 in attributes["ipv4_config"]:
+                last_ipv4 = len(attributes["ipv4_config"]) - 1
 
-                    if ipv4["type"] == "primary":
+                for i in range(len(attributes["ipv4_config"])):
+
+                    if i == 0:
 
                         dst_config.append(
-                            "    set ip " + ipv4["ip_address"] + " " + ipv4["mask"]
+                            cfglvl2
+                            + "set ip "
+                            + attributes["ipv4_config"][i]["ip_address"]
+                            + " "
+                            + attributes["ipv4_config"][i]["mask"]
                         )
 
-                if len(attributes["ipv4_config"]) > 1:
+                    else:
 
-                    dst_config.append("    set secondary-IP enable")
-                    dst_config.append("    config secondaryip")
+                        if i == 1:
 
-                    for ipv4 in attributes["ipv4_config"]:
+                            dst_config.append(cfglvl2 + "set secondary-IP enable")
+                            dst_config.append(cfglvl2 + "config secondaryip")
 
-                        if ipv4["type"] == "secondary":
+                        dst_config.append(cfglvl3 + "edit " + str(i))
+                        dst_config.append(
+                            cfglvl4
+                            + "set ip "
+                            + attributes["ipv4_config"][i]["ip_address"]
+                            + " "
+                            + attributes["ipv4_config"][i]["mask"]
+                        )
+                        dst_config.append(cfglvl3 + "next")
 
-                            dst_config.append("        edit 0")
-                            dst_config.append(
-                                "            set ip "
-                                + ipv4["ip_address"]
-                                + " "
-                                + ipv4["mask"]
-                            )
-                            dst_config.append("        next")
+                        if i == last_ipv4:
 
-                    dst_config.append("    end")
+                            dst_config.append(cfglvl2 + "end")
 
-            dst_config.append("  next")
+            dst_config.append(cfglvl1 + "next")
 
         ### need to add support for sub interfaces and switch interfaces
 
@@ -112,14 +125,14 @@ def generate(logger, parsed_data):
 
     for route_id, attributes in parsed_data["routes"].items():
 
-        dst_config.append("  edit " + str(route_id))
+        dst_config.append(cfglvl1 + "edit " + str(route_id))
         dst_config.append(
-            "    set dst " + attributes["network"] + " " + attributes["mask"]
+            cfglvl2 + "set dst " + attributes["network"] + " " + attributes["mask"]
         )
-        dst_config.append("    set device " + attributes["interface"])
-        dst_config.append("    set gateway " + attributes["gateway"])
-        dst_config.append("    set distance " + attributes["distance"])
-        dst_config.append("  next")
+        dst_config.append(cfglvl2 + "set device " + attributes["interface"])
+        dst_config.append(cfglvl2 + "set gateway " + attributes["gateway"])
+        dst_config.append(cfglvl2 + "set distance " + attributes["distance"])
+        dst_config.append(cfglvl1 + "next")
 
     dst_config.append("end")
 
@@ -131,29 +144,33 @@ def generate(logger, parsed_data):
 
     for address, attributes in parsed_data["network_objects"].items():
 
-        dst_config.append('  edit "' + address + '"')
+        dst_config.append(cfglvl1 + 'edit "' + address + '"')
 
         if attributes["type"] == "host":
 
-            dst_config.append("    set type ipmask")
+            dst_config.append(cfglvl2 + "set type ipmask")
             dst_config.append(
-                "    set subnet " + attributes["host"] + " 255.255.255.255"
+                cfglvl2 + "set subnet " + attributes["host"] + " 255.255.255.255"
             )
 
         elif attributes["type"] == "network":
 
-            dst_config.append("    set type ipmask")
+            dst_config.append(cfglvl2 + "set type ipmask")
             dst_config.append(
-                "    set subnet " + attributes["network"] + " " + attributes["mask"]
+                cfglvl2
+                + "set subnet "
+                + attributes["network"]
+                + " "
+                + attributes["mask"]
             )
 
         elif attributes["type"] == "range":
 
-            dst_config.append("    set type iprange")
-            dst_config.append("    set start-ip " + attributes["address_first"])
-            dst_config.append("    set end-ip " + attributes["address_last"])
+            dst_config.append(cfglvl2 + "set type iprange")
+            dst_config.append(cfglvl2 + "set start-ip " + attributes["address_first"])
+            dst_config.append(cfglvl2 + "set end-ip " + attributes["address_last"])
 
-        dst_config.append("  next")
+        dst_config.append(cfglvl1 + "next")
 
     dst_config.append("end")
 
@@ -175,9 +192,9 @@ def generate(logger, parsed_data):
                 grp_members = grp_members + ' "' + member + '"'
 
             if grp_members:
-                dst_config.append("    set member" + grp_members)
+                dst_config.append(cfglvl2 + "set member" + grp_members)
 
-            dst_config.append("  next")
+            dst_config.append(cfglvl1 + "next")
 
     dst_config.append("end")
 
@@ -195,36 +212,39 @@ def generate(logger, parsed_data):
 
             if attributes["protocol"] in ["1", "icmp", "Icmp", "ICMP"]:
 
-                dst_config.append("    set protocol ICMP")
-                dst_config.append("    set icmptype " + attributes["icmp-type"])
-                dst_config.append("    set icmpcode " + attributes["icmp-code"])
+                dst_config.append(cfglvl2 + "set protocol ICMP")
+                dst_config.append(cfglvl2 + "set icmptype " + attributes["icmp-type"])
+                dst_config.append(cfglvl2 + "set icmpcode " + attributes["icmp-code"])
 
             elif attributes["protocol"] in ["6", "tcp", "Tcp", "TCP"]:
 
-                dst_config.append("    set protocol TCP/UDP/SCTP")
+                dst_config.append(cfglvl2 + "set protocol TCP/UDP/SCTP")
                 dst_config.append(
-                    "    set tcp-portrange " + attributes["destination_port"]
+                    cfglvl2 + "set tcp-portrange " + attributes["destination_port"]
                 )
 
             elif attributes["protocol"] in ["17", "udp", "Udp", "UDP"]:
 
-                dst_config.append("    set protocol TCP/UDP/SCTP")
+                dst_config.append(cfglvl2 + "set protocol TCP/UDP/SCTP")
                 dst_config.append(
-                    "    set udp-portrange " + attributes["destination_port"]
+                    cfglvl2 + "set udp-portrange " + attributes["destination_port"]
                 )
 
             else:
 
-                dst_config.append("    set protocol IP")
-                dst_config.append("    set protocol-number " + attributes["protocol"])
+                dst_config.append(cfglvl2 + "set protocol IP")
+                dst_config.append(
+                    cfglvl2 + "set protocol-number " + attributes["protocol"]
+                )
 
         if attributes["type"] == "range":
 
             if attributes["protocol"] in ["6", "tcp", "Tcp", "TCP"]:
 
-                dst_config.append("    set protocol TCP/UDP/SCTP")
+                dst_config.append(cfglvl2 + "set protocol TCP/UDP/SCTP")
                 dst_config.append(
-                    "    set tcp-portrange "
+                    cfglvl2
+                    + "set tcp-portrange "
                     + attributes["destination_port_first"]
                     + "-"
                     + attributes["destination_port_last"]
@@ -232,15 +252,16 @@ def generate(logger, parsed_data):
 
             elif attributes["protocol"] in ["17", "udp", "Udp", "UDP"]:
 
-                dst_config.append("    set protocol TCP/UDP/SCTP")
+                dst_config.append(cfglvl2 + "set protocol TCP/UDP/SCTP")
                 dst_config.append(
-                    "    set udp-portrange "
+                    cfglvl2
+                    + "set udp-portrange "
                     + attributes["destination_port_first"]
                     + "-"
                     + attributes["destination_port_last"]
                 )
 
-        dst_config.append("  next")
+        dst_config.append(cfglvl1 + "next")
 
     dst_config.append("end")
 
@@ -262,9 +283,9 @@ def generate(logger, parsed_data):
                 grp_members = grp_members + ' "' + member + '"'
 
             if grp_members:
-                dst_config.append("    set member" + grp_members)
+                dst_config.append(cfglvl2 + "set member" + grp_members)
 
-            dst_config.append("  next")
+            dst_config.append(cfglvl1 + "next")
 
     dst_config.append("end")
 
